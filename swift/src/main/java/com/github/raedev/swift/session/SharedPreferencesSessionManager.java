9b94142a7cb961714ae9d0dae06649b1bf6297c6
RAE <raedev@qq.com>
2022-04-27 17:16:47 +0800

    用户信息加密

--- swift/src/main/java/com/github/raedev/swift/session/SharedPreferencesSessionManager.java
+++ swift/src/main/java/com/github/raedev/swift/session/SharedPreferencesSessionManager.java
@@ -5,13 +5,20 @@ import android.content.SharedPreferences;
 
 import androidx.annotation.Nullable;
 
+import com.blankj.utilcode.util.EncryptUtils;
 import com.github.raedev.swift.utils.JsonUtils;
+import com.github.raedev.swift.utils.TextUtils;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * 偏好保存用户信息
  * @author rae
  */
 public class SharedPreferencesSessionManager extends SessionManager {
+
+    private static final byte[] AES_KEY = "4OM5BxgDQp4uHfwe30i1lzTyY4jeNzBJ".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] AES_IV = "7nfFIJNfr1LXtem8".getBytes(StandardCharsets.UTF_8);
 
     /**
      * 用户信息获取比较频繁，作为一个字段去维护
@@ -24,6 +31,8 @@ public class SharedPreferencesSessionManager extends SessionManager {
         super(sessionName);
         this.mSharedPreferences = context.getSharedPreferences(getSessionName(), Context.MODE_PRIVATE);
         this.mUserClass = userClass;
+        // 兼容旧版，移除没有加密的用户信息
+        mSharedPreferences.edit().remove("userInfo").apply();
     }
 
     @Override
@@ -41,11 +50,14 @@ public class SharedPreferencesSessionManager extends SessionManager {
             // 缓存用户信息
             return (T) mUserInfo;
         }
-        String json = mSharedPreferences.getString("userInfo", null);
+        String json = mSharedPreferences.getString("sessionUserInfo", null);
         if (json == null) {
             return null;
         }
-        mUserInfo = JsonUtils.fromJson(json, mUserClass);
+        String decrypt = decrypt(json);
+//        Log.i("rae", "源数据：" + json);
+//        Log.i("rae", "解密后：" + decrypt);
+        mUserInfo = JsonUtils.fromJson(decrypt, mUserClass);
         return (T) mUserInfo;
     }
 
@@ -54,8 +66,24 @@ public class SharedPreferencesSessionManager extends SessionManager {
         if (userInfo == null) {
             return;
         }
-        String json = JsonUtils.toJson(userInfo);
-        mSharedPreferences.edit().putString("userInfo", json).apply();
+        String json = encrypt(JsonUtils.toJson(userInfo));
+        // 加密用户信息
+//        Log.i("Rae", "保存用户信息：" + json);
+        mSharedPreferences.edit().putString("sessionUserInfo", json).apply();
         mUserInfo = userInfo;
+    }
+
+    private String encrypt(String value) {
+        if (TextUtils.isEmpty(value)) {
+            return null;
+        }
+        return EncryptUtils.encryptAES2HexString(value.getBytes(StandardCharsets.UTF_8), AES_KEY, "AES", AES_IV);
+    }
+
+    private String decrypt(String value) {
+        if (TextUtils.isEmpty(value)) {
+            return null;
+        }
+        return new String(EncryptUtils.decryptHexStringAES(value, AES_KEY, "AES", AES_IV), StandardCharsets.UTF_8);
     }
 }
